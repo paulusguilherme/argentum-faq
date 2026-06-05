0e4b1810f1a65bcc078388c4c7da4f444027176b
paulusguilherme <paulusguilherme@gmail.com>
2026-06-05 13:26:27 -0300

    Update faq.js

--- api/faq.js
+++ api/faq.js
@@ -1,5 +1,5 @@
 api/faq.js
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   if (req.method === 'OPTIONS') return res.status(200).end();
 
