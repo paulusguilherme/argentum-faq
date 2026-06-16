fbde23041e00da7edaf3e7ee5b669bff679e2c6c
paulusguilherme <paulusguilherme@gmail.com>
2026-06-16 16:31:59 -0300

    Update faq.js

--- api/faq.js
+++ api/faq.js
@@ -56,7 +56,7 @@ module.exports = async function handler(req, res) {
       };
     }).filter((i) => i.titulo);
 
-    res.setHeader('Cache-Control', 's-maxage=300, stale-while-revalidate');
+    res.setHeader('Cache-Control', 'no-store');
     return res.status(200).json(items);
   } catch (error) {
     return res.status(500).json({ error: error.message });
